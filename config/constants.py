9ba91fb88aa4917ba92ee01289b311629ca52805
Aldar-Efremov1_L <Aldar-Efremov1_L@russiabpost.ru>
2025-06-08 02:07:33 +0300

    добавил кэшик в гитигнор

---
+++ config/constants.py
@@ -0,0 +1,19 @@
+BASE_URL = "https://api.pomidor-stage.ru"
+
+API_HEADERS = {
+    "accept": "application/json",
+    "Content-Type": "application/json"
+}
+
+AUTH_HEADERS = {
+    "accept": "application/json",
+    "Content-Type": "application/x-www-form-urlencoded"
+}
+
+DATA = {
+    "username": "glowww9012@gmail.com",
+    "password": "Trolka2002!",
+    "scope": "",
+    "client_id": "",
+    "client_secret": ""
+}
